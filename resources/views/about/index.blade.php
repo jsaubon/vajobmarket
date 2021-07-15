4c4e5a413f398a90fa7e783af20dee671f9aa383
IsaiahCriz <saicrizofficial@gmail.com>
2021-07-15 21:26:29 +0800

    index-blade-update

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -4,7 +4,7 @@
 <link href="{{URL::asset('assets/css/about.css')}}" rel="stylesheet">
 <section class="aboutSection">
     <div class="container">
-        <div class="card p-4 cardSize text-start">
+        <div class="card shadow p-4 cardSize text-start">
             <h4><b>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat!</b></h4>
             <br/>
             <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ab quasi maiores dolorem sapiente saepe earum, cupiditate veniam rerum repellendus dolore quo facere nobis, repudiandae error asperiores cum vitae natus quia nostrum id voluptate voluptatum. Tempore consectetur sapiente molestias adipisci. Voluptatibus quos soluta corporis ipsam exercitationem iusto repellat repudiandae reprehenderit?</small>
@@ -35,7 +35,7 @@
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae minima ab itaque ad cumque laudantium facilis provident inventore fugiat pariatur excepturi nam aut ratione eum molestiae numquam dolorum ipsam obcaecati, esse amet quasi tenetur quaerat? Qui neque necessitatibus enim consectetur, quam aliquam illo, optio quod possimus sit magnam fuga rem?</p>
                 </div>
             </div>
-            <div class="card p-4 mt-5 mb-5">
+            <div class="card shadow p-4 mt-5 mb-5">
                 <div class="row">
                     <div class="col-sm-6">
 
@@ -48,7 +48,7 @@
         </div>
     </div>
 </section>
-<section class="servicesSection">
+<section class="servicesSection mt-5 mb-5">
     <div class="container">
         <div class="text-center">
             <h1><b>OUR SERVICES</b></h1>
@@ -65,6 +65,52 @@
                     </a>
                 </div>
                 @endforeach
+            </div>
+        </div>
+    </div>
+</section>
+<section class="goalsVision">
+    <div class="container">
+        <br/>
+        <br/>
+        <br/>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="p-5 card-css">
+                    <h1 class="text-center"><b>OUR GOALS</b></h1>
+                    <h5 class="lowtext text-center">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos minus soluta libero hic iusto blanditiis nulla animi eos repudiandae suscipit harum iste adipisci, similique laborum et laudantium. Sapiente iusto vel aliquid praesentium distinctio! Nostrum reiciendis porro praesentium esse, officiis adipisci possimus, quis soluta molestiae saepe est fugit quo ex pariatur!
+                    </h5>
+                    <hr/>
+                    <h1 class="text-center"><b>THE VISION</b></h1>
+                    <h5 class="lowtext text-center">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nobis officiis fugit ad earum iusto rem rerum eligendi aliquam voluptas suscipit voluptates perferendis, quibusdam vitae atque esse maxime recusandae quisquam autem, sed obcaecati. Voluptatem adipisci neque blanditiis cum illo sunt asperiores, nisi alias, tempora aliquid labore fugit tempore debitis doloribus.
+                    </h5>
+                    <hr/>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bossKenSection">
+    <div class="container">
+        <br/>
+        <br/>
+        <br/>
+        <div class="row">
+            <div class="col-sm-6">
+            </div>
+            <div class="col-sm-6">
+                <div class="p-5 card-css">
+                    <h3 class="text-center text-bold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, aliquid?</h3>
+                    <br/>
+                    <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero iusto quas provident dolore amet dicta deserunt et rem mollitia, natus dolor quae ab nesciunt sunt vel cum esse facere quibusdam. Esse necessitatibus natus quidem cupiditate, dolores quas. Nisi illo illum inventore temporibus cumque quo est in, incidunt aut perspiciatis corporis rerum sit aliquam impedit provident saepe accusantium totam, vel esse. Quidem nobis odit eveniet pariatur tenetur rem assumenda molestiae aut?
+                    </p>
+                    <br/>
+                    <b>KENN COLLIER</b><br/>
+                    <small>Chief Executive Officer</small>
+                </div>
             </div>
         </div>
     </div>
