f7812466ed18a8d39f73d957e9deb759b8de6296
Ryan Vincent Cequina <ryanvcequina@gmail.com>
2021-07-23 23:09:03 +0800

    initial commit 7/23/2021

--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,10 @@ Route::get('/jobseekerFinalize', 'Frontend\JobSearchController@jobseekerFinalize
 
 //business-owner
 Route::get('/business', 'Frontend\BusinessController@index');
-Route::get('/businessReview', 'Frontend\BusinessController@businessReview');
 Route::get('/businessPersonalInfo', 'Frontend\BusinessController@businessPersonalInfo');
 Route::get('/businessInfo', 'Frontend\BusinessController@businessInfo');
 Route::get('/BusinessBilling', 'Frontend\BusinessController@BusinessBilling');
+Route::get('/businessReview', 'Frontend\BusinessController@businessReview');
 Route::get('/BusinessFinalize', 'Frontend\BusinessController@BusinessFinalize');
 Route::get('/BusinessProfile', 'Frontend\BusinessController@BusinessProfile');
 Route::get('/BusinessProfilePersonal', 'Frontend\BusinessController@BusinessProfilePersonal');
