72489ddc181ec7bf266ea88726d0fb97c4832e2b
joshua saubon <joshuasaubon@joshuas-Mac-mini.local>
2021-07-24 17:44:09 +0800

    fix

--- resources/views/layouts/profile-layout.blade.php
+++ resources/views/layouts/profile-layout.blade.php
@@ -15,82 +15,8 @@
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
 
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-        <script>
-        let token = localStorage.token;
-        let userdata = localStorage.userdata ? JSON.parse(localStorage.userdata) : null;
-
-        function getData(url, then_function = () => {}, catch_function = () => {}) {
-            axios.get(url,{
-                headers: {
-                    Authorization: "Bearer " + token
-                }
-            }).then(then_function).catch(catch_function);
-        };
-
-        function postData(url,data, then_function = () => {}, catch_function = () => {}) {
-            axios.post(url, data,{
-                headers: {
-                    Authorization: "Bearer " + token
-                }
-            }).then(then_function).catch(catch_function);
-        };
-
-        function uploadImage(url,data, then_function = () => {}, catch_function = () => {}) {
-            axios.put(url, data,{
-                headers: {
-                    Authorization: "Bearer " + token,
-                    "Content-Type": "multipart/form-data"
-                }
-            }).then(then_function).catch(catch_function);
-        };
-
-        function updateData(url,data, then_function = () => {}, catch_function = () => {}) {
-            axios.put(url, data,{
-                headers: {
-                    Authorization: "Bearer " + token
-                }
-            }).then(then_function).catch(catch_function);
-        };
-
-        function deleteData(url,data, then_function = () => {}, catch_function = () => {}) {
-            axios.delete(url, data,{
-                headers: {
-                    Authorization: "Bearer " + token
-                }
-            }).then(then_function).catch(catch_function);
-        };
-
-        $.fn.serializeControls = function() {
-            var data = {};
-
-            function buildInputObject(arr, val) {
-                if (arr.length < 1)
-                return val;  
-                var objkey = arr[0];
-                if (objkey.slice(-1) == "]") {
-                objkey = objkey.slice(0,-1);
-                }  
-                var result = {};
-                if (arr.length == 1){
-                result[objkey] = val;
-                } else {
-                arr.shift();
-                var nestedVal = buildInputObject(arr,val);
-                result[objkey] = nestedVal;
-                }
-                return result;
-            }
-
-            $.each(this.serializeArray(), function() {
-                var val = this.value;
-                var c = this.name.split("[");
-                var a = buildInputObject(c, val);
-                $.extend(true, data, a);
-            });
-            
-            return data;
-        }
-    </script>
+        
+        @include('layouts.scriptFunctions')
     </head>
     <body>
         @include('profile.nav-profile')
