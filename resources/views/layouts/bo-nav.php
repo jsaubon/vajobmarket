7b9af8eeff787d966a88e20753fb924169fa6b26
joshua saubon <jsaubon@joshuas-MacBook-Air.local>
2021-07-27 01:34:39 +0800

    fix

---
+++ resources/views/layouts/bo-nav.php
@@ -0,0 +1,67 @@
+<div class="header-items-right bo-nav">
+    <div  class="px-2 py-1 ml-3 postJobBtn d-flex justify-content-around" style="margin:0 !important; width:95px;color:#383232 ">
+        <i class="fas fa-plus-circle my-auto" style="font-size:12px;" ></i>&nbsp;
+        <b class="my-auto">Post A Job</b>
+    </div>
+    <div  class="p-1 noDeco ml-2" style="color:#383232;">
+        <i class="fas fa-bell noDeco shadow" style="font-size:15px;"></i>
+    </div>
+    <div  class="p-1  noDeco ml-2" style="color:#383232;">
+        <i class="fas fa-user noDeco shadow" style="font-size:15px;"></i>
+    </div>
+    <div class="p-1 ml-3 mr-3 noDeco user-btn user-btn-bo">
+        <small><b class="my-auto user-firstname ">Username</b></small>
+        <i class="fas fa-chevron-down my-auto" style="font-size:10px;"></i>
+    </div>
+</div>
+<div class="small-nav small-nav-bo shadow bo-nav">
+    <ul>
+        <a href="#" class="small-nav-link">
+            <li class="small-nav-list">
+                dashboard
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="/BusinessProfile" class="small-nav-link">
+            <li class="small-nav-list">
+                profile
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="/BusinessAllJobPost" class="small-nav-link">
+            <li class="small-nav-list">
+                Job Posts
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="/BusinessApplications" class="small-nav-link">
+            <li class="small-nav-list">
+                application
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="/BusinessEmployment" class="small-nav-link">
+            <li class="small-nav-list">
+                employment
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="#" class="small-nav-link">
+            <li class="small-nav-list">
+                message
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="/BusinessProfileCompany" class="small-nav-link">
+            <li class="small-nav-list">
+                settings
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="#" class="small-nav-link btn-logout">
+            <li class="small-nav-list">
+                logout
+            </li>
+        </a>
+    </ul>
+</div>
---
+++ resources/views/layouts/js-nav.php
@@ -0,0 +1,59 @@
+<div class="header-items-right js-nav">
+    <a href="/jobs" class="p-1 ml-3 postJobBtn noDeco"><small><b>JOB SEARCH</b></small></a>
+    <div  class="p-1 noDeco ml-2" style="color:#383232;">
+        <i class="fas fa-bell noDeco shadow" style="font-size:15px;"></i>
+    </div>
+    <div  class="p-1  noDeco ml-2" style="color:#383232;">
+        <i class="fas fa-user noDeco shadow" style="font-size:15px;"></i>
+    </div>
+    <div class="p-1 ml-3 mr-3 noDeco user-btn user-btn-js">
+        <small><b class="my-auto user-firstname ">Username</b></small>
+        <i class="fas fa-chevron-down my-auto" style="font-size:10px;"></i>
+    </div>
+</div>
+<div class="small-nav-js small-nav shadow js-nav">
+    <ul>
+        <a href="/jobseekerDashboard" class="small-nav-link">
+            <li class="small-nav-list">
+                dashboard
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="/jobseekerProfile" class="small-nav-link">
+            <li class="small-nav-list">
+                profile
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="#" class="small-nav-link">
+            <li class="small-nav-list">
+                job application
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="#" class="small-nav-link">
+            <li class="small-nav-list">
+                employment
+            </li>
+        </a>
+        <hr class=" m-0">
+        <hr class=" m-0">
+        <a href="#" class="small-nav-link">
+            <li class="small-nav-list">
+                messages
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="/jobseekerUpdateDetails" class="small-nav-link">
+            <li class="small-nav-list">
+                settings
+            </li>
+        </a>
+        <hr class=" m-0">
+        <a href="#" class="small-nav-link btn-logout">
+            <li class="small-nav-list">
+                logout
+            </li>
+        </a>
+    </ul>
+</div>
